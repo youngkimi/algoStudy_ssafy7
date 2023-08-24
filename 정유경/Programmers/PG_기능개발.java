6fd707c8560c69ec0ee76708f34e7c88ae8e9f88
정유경 <kadie@naver.com>
2023-08-24 11:52:57 +0900

    [정유경]PG_기능개발

---
+++ 정유경/Programmers/PG_기능개발.java
@@ -0,0 +1,26 @@
+package Programmers;
+
+import java.util.*;
+
+public class PG_기능개발 {
+	public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<progresses.length; i++){
+            queue.add((int) Math.ceil((100.0 - progresses[i]) / speeds[i]));
+        }
+        
+        List<Integer> answer = new ArrayList<>();
+        while (!queue.isEmpty()){
+            int day = queue.poll();
+            int cnt = 1;
+            
+            while(!queue.isEmpty() && day >= queue.peek()){
+                cnt++;
+                queue.poll();
+            }
+            answer.add(cnt);
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
