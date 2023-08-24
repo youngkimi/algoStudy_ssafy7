7de7e80484c1ec502c99d36c272b827bb0e19aa2
정유경 <kadie@naver.com>
2023-08-24 11:51:55 +0900

    [정유경]PG_같은숫자는싫어

---
+++ 정유경/Programmers/PG_같은숫자는싫어.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+import java.util.Stack;
+
+public class PG_같은숫자는싫어 {
+	public int[] solution(int[] arr) {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(arr[0]);
+		for(int i=1; i<arr.length; i++) {
+			if(stack.peek() != arr[i]) {
+				stack.push(arr[i]);
+			}
+		}
+		
+		int[] answer = new int[stack.size()];
+		for(int i=stack.size()-1; i>=0; i--) {
+			answer[i] = stack.pop();
+		}
+		return answer;
+	}
+}
